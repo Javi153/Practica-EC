52808698eaa1388a3262dfb371e62d56f87ea026
Javi153 <jjimen06@ucm.es>
2022-03-25 01:01:50 +0100

    Primer push

---
+++ workspace_v11/Practica3_1/Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: F:/Universidad/Tercero/CodeComposer/ccs/ccs_base/msp430/include/msp430.h
+main.obj: F:/Universidad/Tercero/CodeComposer/ccs/ccs_base/msp430/include/msp430fr6989.h
+main.obj: F:/Universidad/Tercero/CodeComposer/ccs/ccs_base/msp430/include/in430.h
+main.obj: F:/Universidad/Tercero/CodeComposer/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: F:/Universidad/Tercero/CodeComposer/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c:
+
+F:/Universidad/Tercero/CodeComposer/ccs/ccs_base/msp430/include/msp430.h:
+
+F:/Universidad/Tercero/CodeComposer/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+F:/Universidad/Tercero/CodeComposer/ccs/ccs_base/msp430/include/in430.h:
+
+F:/Universidad/Tercero/CodeComposer/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+F:/Universidad/Tercero/CodeComposer/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
